38350d6a8bba2b90ae380333ba9692fe2784c118
VamsiKrishna-Maddali <100993952+VamsiKrishna-Maddali@users.noreply.github.com>
2022-03-09 19:48:15 +0000

    Update RPS-Template.py

--- RPS-Template.py
+++ RPS-Template.py
@@ -1,9 +1,13 @@
 import cv2
+import random
 from keras.models import load_model
 import numpy as np
-model = load_model('YOUR_MODEL.h5')
+model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+choice = ["Rock", "Paper", "Scissors", "Nothing"]
+print (choice[3])
 
 while True: 
     ret, frame = cap.read()
@@ -12,6 +16,14 @@ while True:
     normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
     data[0] = normalized_image
     prediction = model.predict(data)
+    if prediction[0][0] > 0.5:
+        print ("Rock")
+    elif prediction[0][1] > 0.5:
+        print ("Paper")
+    elif prediction[0][2] > 0.5:
+        print ("Scissors")
+    else:
+        print ("Nothing")
     cv2.imshow('frame', frame)
     # Press q to close the window
     print(prediction)
